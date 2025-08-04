7d34715afa1c6e9377ae210d96ad22affb3fba17
Samy Elsayed <samyelsayed@gmail.com>
2025-08-04 23:47:33 +0300

    any comment

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -10,28 +10,28 @@ export default function Category() {
       <div className="row g-4 justify-content-center p-4">
         <div className="card col-lg-3 col-md-6 col-sm-12 border-0 shadow-sm">
           <div className="details d-flex flex-column align-items-center justify-content-center p-4">
-            <i className="icons fa-solid fa-check text-warning fs-2 mb-2"></i>
+            <i className="icons fa-solid fa-check  fs-2 mb-2"></i>
             <p className="fs-5 fw-semibold m-0">Quality Product</p>
           </div>
         </div>
 
         <div className="card col-lg-3 col-md-6 col-sm-12 border-0 shadow-sm">
           <div className="details d-flex flex-column align-items-center justify-content-center p-4">
-            <i className="icons fa-solid fa-truck text-warning fs-2 mb-2"></i>
+            <i className="icons fa-solid fa-truck  fs-2 mb-2"></i>
             <p className="fs-5 fw-semibold m-0">Free Shipping</p>
           </div>
         </div>
 
         <div className="card col-lg-3 col-md-6 col-sm-12 border-0 shadow-sm">
           <div className="details d-flex flex-column align-items-center justify-content-center p-4">
-            <i className="icons fa-solid fa-rotate-left text-warning fs-2 mb-2"></i>
+            <i className="icons fa-solid fa-rotate-left  fs-2 mb-2"></i>
             <p className="fs-5 fw-semibold m-0">14-Day Return</p>
           </div>
         </div>
 
         <div className="card col-lg-3 col-md-6 col-sm-12 border-0 shadow-sm">
           <div className="details d-flex flex-column align-items-center justify-content-center p-4">
-            <i className="icons fa-solid fa-headset text-warning fs-2 mb-2"></i>
+            <i className="icons fa-solid fa-headset  fs-2 mb-2"></i>
             <p className="fs-5 fw-semibold m-0">24/7 Support</p>
           </div>
         </div>
